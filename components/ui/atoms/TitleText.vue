082bbe28dd34e8c0f1a2f0aadee4d32fcd2317dd
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-09-30 18:50:35 +0200

    feat: created title text component

---
+++ components/ui/atoms/TitleText.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    buttonLabel?: string
+    buttonLink?: string
+    center?: boolean
+    sprite?: string
+  }>(), {
+    center: false,
+    buttonLabel: '',
+    buttonLink: '',
+    sprite: undefined,
+  },
+)
+</script>
+
+<template>
+  <div
+    class="group"
+    :class="{ 'text-center': center }"
+  >
+    <h2
+      v-if="$slots.default"
+      class="pb-4 relative max-w-max"
+      :class="{ 'mx-auto': center }"
+    >
+      <slot />
+      <img
+        v-if="sprite"
+        :src="`/art/${sprite}.svg`"
+        loading="lazy"
+        class="w-10 h-10 absolute -right-12 -top-3 z-[-1] group-hover:rotate-12 transition-transform group-hover:scale-[1.2]"
+      >
+    </h2>
+    <p
+      v-if="$slots.text"
+      class="max-w-prose"
+      :class="{ 'mx-auto': center }"
+    >
+      <slot name="text" />
+    </p>
+    <div
+      v-if="buttonLink && buttonLabel"
+      class="flex mt-6"
+      :class="[center ? 'justify-center' : 'justify-start']"
+    >
+      <RouteLink :url="buttonLink">
+        <button
+          class="w-fit btn-primary"
+          :aria-label="buttonLabel"
+        >
+          {{ buttonLabel }}
+        </button>
+      </RouteLink>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -51,18 +51,39 @@ if (import.meta.client) {
       class="hidden md:block -z-[1] h-[200px] w-[200px] fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full from-info to-secondary bg-gradient-to-r rotates blur-[100px]"
     />
     <Hero />
-    <LazySummary
-      :title="t('pages.home.summary.title')"
-      sprite="flame"
-      :items="[
-        t('pages.home.summary.item1'),
-        t('pages.home.summary.item2'),
-        t('pages.home.summary.item3'),
-        t('pages.home.summary.item4'),
-        t('pages.home.summary.item5'),
-        t('pages.home.summary.item6'),
-      ]"
-      class="max-w-prose"
-    />
+    <div class="flex flex-col gap-y-[100px] sm:gap-y-[150px] pb-20 pt-28 relative">
+      <div
+        data-skills
+        class="space-y-20 dnd-container"
+        @mousemove="dragon.calculateEyes"
+      >
+        <LazySummary
+          :title="t('pages.home.summary.title')"
+          sprite="flame"
+          :items="[
+            t('pages.home.summary.item1'),
+            t('pages.home.summary.item2'),
+            t('pages.home.summary.item3'),
+            t('pages.home.summary.item4'),
+            t('pages.home.summary.item5'),
+            t('pages.home.summary.item6'),
+          ]"
+          class="max-w-prose"
+        />
+        <LazyTitleText
+          button-link="playground"
+          :button-label="t('pages.home.textBlock1.button')"
+          sprite="hearth"
+        >
+          {{ t('pages.home.textBlock1.title') }}
+          <template #text>
+            {{ t('pages.home.textBlock1.text') }}
+            <span class="mt-3">
+              {{ t('pages.home.textBlock1.text2') }}
+            </span>
+          </template>
+        </LazyTitleText>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
