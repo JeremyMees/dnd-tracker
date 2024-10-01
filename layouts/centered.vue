dffc263cb2ce95249a2ce4f1fc55ec8f91fbd2af
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-10-01 20:56:11 +0200

    feat: added login page

---
+++ layouts/centered.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const localePath = useLocalePath()
+const ui = useUI()
+
+const blob = ref<HTMLDivElement>()
+
+onBeforeMount(() => ui.showNavigation = false)
+
+if (import.meta.client) {
+  document.body.onmousemove = (event) => {
+    if (blob.value) {
+      const { clientX, clientY } = event
+
+      blob.value.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`,
+      }, { duration: 3000, fill: 'forwards' })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="min-h-screen relative overflow-hidden">
+    <div
+      ref="blob"
+      class="hidden md:block -z-[1] h-[200px] w-[200px] fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full from-info to-secondary bg-gradient-to-r rotates blur-[100px]"
+    />
+    <NuxtLink
+      class="absolute inset-0 bg-transparent cursor-pointer"
+      :to="localePath('/')"
+    />
+    <div
+      class="container absolute w-full max-w-2xl -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2"
+    >
+      <div class="inset-0 z-[-0] fancy-shadow" />
+      <div
+        class="bg-tracker/50 px-2 py-6 sm:p-8 rounded-lg relative border-4 border-tracker"
+      >
+        <img
+          src="/gifs/wolf-rider.gif"
+          loading="lazy"
+          class="w-8 h-8 absolute -top-8"
+        >
+        <NuxtLink
+          :to="localePath('/')"
+          class="group"
+        >
+          <Icon
+            name="ic:round-clear"
+            class="w-8 h-8 text-danger cursor-pointer absolute right-4 top-4 rounded-full ring-danger group-focus-within:ring"
+            aria-hidden="true"
+          />
+        </NuxtLink>
+        <div class="px-4 sm:px-8">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,88 @@
+<script setup lang="ts">
+useHead({ title: 'Log in' })
+
+const auth = useAuth()
+const { t } = useI18n()
+
+const form = ref<Login>({ email: '', password: '' })
+const isLoading = ref<boolean>(false)
+const error = ref<string | null>(null)
+
+async function login({ __init, isTrusted, _vts, ...credentials }: Obj): Promise<void> {
+  error.value = null
+  try {
+    isLoading.value = true
+    await auth.login(credentials as Login)
+  }
+  catch (err: any) {
+    console.error(err)
+    error.value = err.message
+  }
+  finally {
+    isLoading.value = false
+  }
+}
+</script>
+
 <template>
-  <div>
-    login
-  </div>
+  <NuxtLayout name="centered">
+    <section class="space-y-6">
+      <h1 class="text-center">
+        {{ t('pages.login.title') }}
+      </h1>
+      <NuxtImg
+        src="/classes.png"
+        alt="D20 logo dice"
+        :width="250"
+        :height="250"
+        class="mx-auto visibility-pulse"
+      />
+      <p
+        v-if="error"
+        class="text-danger text-center body-small"
+      >
+        {{ error }}
+      </p>
+      <FormKit
+        v-model="form"
+        type="form"
+        :actions="false"
+        @submit="login"
+      >
+        <FormKit
+          name="email"
+          :label="t('components.inputs.emailLabel')"
+          validation="required|length:5,50|email"
+        />
+        <FormKit
+          name="password"
+          type="password"
+          :label="t('components.inputs.passwordLabel')"
+          validation="required|length:6,50"
+        />
+        <FormKit
+          type="submit"
+          :aria-label="t('pages.login.signIn')"
+          :label="t('pages.login.signIn')"
+          :disabled="isLoading"
+          outer-class="$reset grow"
+          input-class="w-full"
+        />
+      </FormKit>
+      <div class="flex flex-wrap gap-2 justify-center">
+        <RouteLink
+          url="register"
+          class="btn-text"
+        >
+          {{ t('pages.login.new') }}
+        </RouteLink>
+        <RouteLink
+          url="forgot-password"
+          class="btn-text"
+        >
+          {{ t('pages.login.forgot') }}
+        </RouteLink>
+      </div>
+    </section>
+  </NuxtLayout>
 </template>
