524bc6caea4936a34bb89c6f986abefd53e52225
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-10-27 16:03:50 +0100

    feat: random array items and shuffle array functions

--- utils/array-utils.ts
+++ utils/array-utils.ts
@@ -2,6 +2,22 @@ export function randomArrayItem<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)]
 }
 
+export function randomArrayItems<T>(arr: T[], number = 1): T[] {
+  const result: T[] = []
+  const copy = [...arr]
+
+  if (number >= arr.length) {
+    return shuffleArray(arr)
+  }
+
+  for (let i = 0; i < number; i++) {
+    const index = Math.floor(Math.random() * copy.length)
+    result.push(copy.splice(index, 1)[0])
+  }
+
+  return result
+}
+
 export function searchArray<T>(arr: T[], key: keyof T, search: string): T[] {
   return arr
     .filter(item => (item[key] as string)
@@ -15,6 +31,17 @@ export function toggleSelection<T extends { id: number }>(item: T, selected: T[]
 
   if (index === -1) selected.push(item)
   else selected.splice(index, 1)
+}
+
+export function shuffleArray<T>(arr: T[]): T[] {
+  for (let i = 0; i < arr.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+
+  return arr
 }
 
 export function splitArray<T>(arr: T[], size: number): T[][] {
