80c8f88908fba666eb2ba5ccaa0ddf0516230492
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-11-30 22:05:07 +0100

    feat: no access page

--- pages/no-access.vue
+++ pages/no-access.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 useSeo('No access')
+
+const { t } = useI18n()
 </script>
 
 <template>
   <NuxtLayout name="centered">
-    No access
+    <h2>
+      {{ t('pages.noAccess.title') }}
+    </h2>
+    <p class="mt-4">
+      {{ t('pages.noAccess.text') }}
+    </p>
   </NuxtLayout>
 </template>
