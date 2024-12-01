b0a40f948e6cdef65fdd82b712103b5b16826c04
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-12-01 14:49:47 +0100

    fix: added http method in server route name

--- server/api/user/remove.post.ts
+++ server/api/user/remove.post.ts
@@ -1,7 +1,8 @@
 import { serverSupabaseServiceRole } from '#supabase/server'
+import type { Database } from '~/types/database'
 
 export default defineEventHandler(async (event) => {
-  const supabase = serverSupabaseServiceRole(event)
+  const supabase = serverSupabaseServiceRole<Database>(event)
   const body: { id: string } = await readBody(event)
 
   return await supabase.auth.admin.deleteUser(body.id)
