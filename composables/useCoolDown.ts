d585d94708bf8b8cdd7a8f967304c8e3097cc28b
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-12-27 13:20:07 +0100

    feat: use cooldown function

---
+++ composables/useCoolDown.ts
@@ -0,0 +1,36 @@
+type CoolDowns = Record<string, number>
+
+export function useCoolDown() {
+  const coolDowns: CoolDowns = reactive({})
+
+  const startCoolDown = (id: number, seconds: number) => {
+    if (!coolDowns[id]) {
+      coolDowns[id] = seconds
+
+      const interval = setInterval(() => {
+        if (coolDowns[id] > 0) {
+          coolDowns[id] -= 1
+        }
+        else {
+          clearInterval(interval)
+          delete coolDowns[id]
+        }
+      }, 1000)
+    }
+  }
+
+  const isInCoolDown = (id: number): boolean => {
+    return !!coolDowns[id] && coolDowns[id] > 0
+  }
+
+  const getRemainingTime = (id: number) => {
+    return coolDowns[id] ? coolDowns[id] : 0
+  }
+
+  return {
+    startCoolDown,
+    isInCoolDown,
+    getRemainingTime,
+    coolDowns,
+  }
+}
