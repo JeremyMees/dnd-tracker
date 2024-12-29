e729e9ca923af837799871ef907eb65ead2c5c27
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-12-29 14:43:39 +0100

    feat: custom error page

---
+++ error.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="flex flex-col gap-y-6 items-center max-w-prose px-8 text-center">
+      <div class="text-[100px] font-black">
+        {{ error!.statusCode }}
+      </div>
+      <h1 class="text-primary text-5xl">
+        {{ $t('pages.error.404') }}
+      </h1>
+      <p>
+        {{ $t('pages.error.text') }}
+      </p>
+      <div class="flex flex-wrap gap-x-4 pb-10">
+        <NuxtLinkLocale
+          to="/"
+          class="w-fit btn-primary"
+        >
+          {{ $t('pages.error.goHome') }}
+        </NuxtLinkLocale>
+      </div>
+    </div>
+  </div>
+</template>
