5e826e1975dd32e342039119e8a46d5a8c97dd6b
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-12-30 09:30:07 +0100

    feat: initiative sheet pet

---
+++ components/initiative/Pet.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  pet: 'cat' | 'chicken' | 'barmaid' | 'crawler' | 'dragon' | 'fairy' | 'redcap' | 'wolf-rider'
+}>()
+</script>
+
+<template>
+  <img
+    :src="`/gifs/${pet}.gif`"
+    loading="lazy"
+    class="size-8"
+  >
+</template>
