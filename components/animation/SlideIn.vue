d45d03a9aecb4cb68ba74fc1bd0dfa528a7bb5bf
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-01-02 15:12:35 +0100

    feat: slide in animation updated to accept position prop

--- components/animation/SlideIn.vue
+++ components/animation/SlideIn.vue
@@ -1,11 +1,28 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    position: Position
+  }>(), {
+    position: 'top',
+  },
+)
+
+const animationClass = computed<string>(() => {
+  if (props.position === 'top') return '-translate-y-full'
+  else if (props.position === 'bottom') return 'translate-y-full'
+  else if (props.position === 'left') return '-translate-x-full'
+  else return 'translate-x-full'
+})
+</script>
+
 <template>
   <Transition
     enter-active-class="duration-300 ease-in-out"
-    enter-from-class="-translate-y-full opacity-0"
+    :enter-from-class="`!${animationClass} opacity-0`"
     enter-to-class="translate-0 opacity-100"
     leave-active-class="duration-300 ease-in-out"
     leave-from-class="translate-0 opacity-100"
-    leave-to-class="-translate-y-full opacity-0"
+    :leave-to-class="`!${animationClass} opacity-0`"
   >
     <slot />
   </Transition>
