919a69ed87128c5dd5cade5e83d9a2996fd6b4dd
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-01-24 23:11:32 +0100

    feat: color mode switcher

---
+++ components/atoms/ColorModeSwitcher.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center justify-between gap-2">
+    <Icon
+      name="tabler:palette"
+      class="size-4 min-w-4"
+    />
+    <UiSelect v-model="$colorMode.preference">
+      <UiSelectTrigger>
+        <UiSelectValue />
+      </UiSelectTrigger>
+      <UiSelectContent>
+        <UiSelectGroup>
+          <UiSelectItem value="light">
+            Light
+          </UiSelectItem>
+          <UiSelectItem value="dark">
+            Dark
+          </UiSelectItem>
+          <UiSelectItem value="system">
+            System
+          </UiSelectItem>
+        </UiSelectGroup>
+      </UiSelectContent>
+    </UiSelect>
+  </div>
+</template>
