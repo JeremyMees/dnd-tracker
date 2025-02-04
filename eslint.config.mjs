36c2b006f7bdc2d5cbe383e3d0569180acacf11c
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-02-04 21:50:35 +0100

    feat: added tanstack eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,23 @@
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
-  files: ['**/*.js', '**/*.ts', '**/*.vue'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-dynamic-delete': 'off',
-    '@typescript-eslint/no-empty-object-type': 'off',
-    'camelcase': 0,
-    'vue/no-side-effects-in-computed-properties': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/no-mutating-props': 'off',
-    'vue/no-v-html': 'off',
-    'no-console': ['error', {
-      allow: ['info', 'warn', 'trace', 'error'],
-    }],
+export default withNuxt(
+  ...pluginQuery.configs['flat/recommended'],
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.vue'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-dynamic-delete': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'camelcase': 0,
+      'vue/no-side-effects-in-computed-properties': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-mutating-props': 'off',
+      'vue/no-v-html': 'off',
+      'no-console': ['error', {
+        allow: ['info', 'warn', 'trace', 'error'],
+      }],
+    },
   },
-})
+)
