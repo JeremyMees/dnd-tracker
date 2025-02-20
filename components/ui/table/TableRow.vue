57275bb81017f801565238c43a15339c70c8f886
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-02-20 11:16:08 +0100

    feat: shadcn table

---
+++ components/ui/table/TableRow.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/utils/shadcn'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <tr :class="cn('border-b border-secondary transition-colors hover:bg-secondary/70 data-[state=selected]:bg-secondary', props.class)">
+    <slot />
+  </tr>
+</template>
