7c1e796b31cfb33725c1b8647a371b5513269927
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-02-20 16:27:57 +0100

    feat: animated loading of content count component

--- components/atoms/ContentCount.vue
+++ components/atoms/ContentCount.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-defineProps<{ count: number, max: number }>()
+defineProps<{
+  count?: number
+  max: number
+  loading?: boolean
+}>()
 </script>
 
 <template>
+  <UiSkeleton
+    v-if="loading || !count"
+    class="w-10 h-4"
+  />
   <span
+    v-else-if="count"
     class="text-[12px] text-muted-foreground"
     :class="{ '!text-destructive': count >= max }"
   >
