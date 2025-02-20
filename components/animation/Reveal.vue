6dfaa8169c4cd80caee6be153bec459052f35fc6
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-02-20 11:14:29 +0100

    fix: reveal animation in and out

--- components/animation/Reveal.vue
+++ components/animation/Reveal.vue
@@ -14,7 +14,9 @@ withDefaults(
       enter-active-class="transition-all duration-300 ease-out"
       :enter-from-class="`opacity-0 ${reverse ? 'translate-y-full' : '-translate-y-full'}`"
       enter-to-class="opacity-100 translate-y-0"
-      leave-active-class="opacity-0"
+      leave-active-class="transition-all duration-300 ease-out"
+      leave-from-class="opacity-100 translate-y-0"
+      :leave-to-class="`opacity-0 ${reverse ? 'translate-y-full' : '-translate-y-full'}`"
     >
       <slot />
     </Transition>
