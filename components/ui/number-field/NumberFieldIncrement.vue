5d7f23372ebe7d5928efb45e965cc176ab22c468
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-03-20 16:25:17 +0100

    feat: label & number input shadcn

---
+++ components/ui/number-field/NumberFieldIncrement.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { NumberFieldIncrementProps } from 'reka-ui'
+import { NumberFieldIncrement, useForwardProps } from 'reka-ui'
+import { computed, type HTMLAttributes } from 'vue'
+import { cn } from '@/utils/shadcn'
+
+const props = defineProps<NumberFieldIncrementProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <NumberFieldIncrement
+    data-slot="increment"
+    v-bind="forwarded"
+    :class="cn('absolute top-1/2 -translate-y-1/2 right-0 disabled:cursor-not-allowed disabled:opacity-20 p-3', props.class)"
+  >
+    <slot>
+      <Icon
+        name="tabler:plus"
+        class="size-4"
+      />
+    </slot>
+  </NumberFieldIncrement>
+</template>
