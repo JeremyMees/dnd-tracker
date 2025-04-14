7af9299c1c9cb16e58cc818d8cbb0dc5a7d789ad
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-04-14 17:02:30 +0200

    feat: added inspiria container scroll

---
+++ components/ui/container-scroll/ContainerScrollCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :style="{
+      transform: `rotateX(${rotate}deg) scale(${scale})`,
+    }"
+    class="mx-auto -mt-12 h-[30rem] w-full max-w-5xl rounded-[30px] border-4 border-border bg-border/50 p-2 shadow-2xl md:h-[40rem] md:p-6"
+  >
+    <div class="size-full overflow-hidden">
+      <slot />
+    </div>
+    <UiBorderBeam
+      :size="350"
+      :duration="12"
+      :border-width="4"
+      color-from="#7434E3"
+      color-to="#9B4BB3"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  rotate: Number,
+  scale: Number,
+})
+</script>
