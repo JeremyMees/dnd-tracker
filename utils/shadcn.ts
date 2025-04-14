eb7884ebb4b4f6e2b6cdb39b035585e370f4913c
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-04-14 17:01:54 +0200

    feat: added inspiria globe

--- utils/shadcn.ts
+++ utils/shadcn.ts
@@ -2,6 +2,8 @@ import type { Updater } from '@tanstack/vue-table'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+export type ObjectValues<T> = T[keyof T]
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
