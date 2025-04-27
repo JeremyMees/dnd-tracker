a15815080f65fb0ebb1cec5fce9e875d6f06f57e
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-04-27 17:10:54 +0200

    feat: updated error page

--- error.vue
+++ error.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Motion } from 'motion-v'
 import type { NuxtError } from '#app'
 
 defineProps({
@@ -9,13 +10,13 @@ defineProps({
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <div class="flex flex-col gap-y-6 items-center max-w-prose px-8 text-center">
-      <div class="text-[100px] font-black">
+      <div class="text-[100px] font-black pb-10">
         {{ error!.statusCode }}
       </div>
       <h1 class="text-primary">
         {{ $t('pages.error.404') }}
       </h1>
-      <p>
+      <p class="text-muted-foreground">
         {{ $t('pages.error.text') }}
       </p>
       <div class="flex flex-wrap gap-x-4 pb-10">
@@ -26,6 +27,29 @@ defineProps({
           {{ $t('pages.error.goHome') }}
         </NuxtLinkLocale>
       </div>
+    </div>
+    <div class="fixed bottom-0 w-full">
+      <Motion
+        :initial="{ y: '100%' }"
+        :animate="{
+          y: '0%',
+          transition: {
+            duration: 0.8,
+            ease: 'easeOut',
+          },
+        }"
+      >
+        <div class="max-w-[300px] md:max-w-[500px] mx-auto">
+          <NuxtImg
+            src="/peeker.png"
+            alt="Dragon peeking from the bottom"
+            sizes="sm:300px md:300px lg:300px"
+            width="2468"
+            height="1398"
+            class="w-full h-full object-cover"
+          />
+        </div>
+      </Motion>
     </div>
   </div>
 </template>
