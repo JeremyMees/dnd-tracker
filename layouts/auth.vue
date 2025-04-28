37286ebd94bfcd8762bf42c9c353500bef01c522
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-04-28 16:08:27 +0200

    feat: new auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="grid min-h-svh lg:grid-cols-2">
+    <div class="flex flex-col gap-4 p-6 md:p-10">
+      <div class="flex justify-center gap-2 md:justify-start">
+        <NuxtLinkLocale to="/">
+          <NuxtImg
+            src="/logo.svg"
+            alt="Image"
+            width="100"
+            height="100"
+          />
+        </NuxtLinkLocale>
+      </div>
+      <div class="flex flex-1 items-center justify-center">
+        <div class="w-full max-w-xs">
+          <slot />
+        </div>
+      </div>
+    </div>
+    <div class="relative hidden bg-muted lg:flex flex-col items-center justify-center">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
