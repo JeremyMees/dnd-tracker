0e5ae7cac08b994a35acb29d47f6c9d8e57dbf39
Jeremy Mees <JeremyMees@users.noreply.github.com>
2025-11-06 16:06:10 +0100

    feat: added shadcn radio group

---
+++ app/components/ui/radio-group/RadioGroupItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { RadioGroupItemProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from 'reka-ui'
+
+const props = defineProps<RadioGroupItemProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <RadioGroupItem
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'peer aspect-square size-4 min-w-4 rounded-full border border-foreground text-foreground ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        props.class,
+      )
+    "
+  >
+    <RadioGroupIndicator
+      class="flex items-center justify-center"
+    >
+      <Icon
+        name="tabler:circle-filled"
+        class="size-3 min-w-3 text-primary relative bottom-0.25"
+      />
+    </RadioGroupIndicator>
+  </RadioGroupItem>
+</template>
