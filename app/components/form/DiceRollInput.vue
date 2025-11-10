26ee631959abea889d969c046d1a3c25285adfa6
Jeremy Mees <JeremyMees@users.noreply.github.com>
2025-11-10 15:36:12 +0100

    feat: updated diceroll input to vee validate

--- app/components/form/DiceRollInput.vue
+++ app/components/form/DiceRollInput.vue
@@ -1,23 +1,37 @@
 <script setup lang="ts">
-const amount = ref<number>()
-const isRollingShown = ref<boolean>(false)
+const isRollingShown = shallowRef(false)
 </script>
 
 <template>
-  <FormKit
-    v-model="amount"
+  <UiFormField
+    v-slot="{ componentField, setValue }"
     name="amount"
-    number
-    type="number"
-    :label="$t('components.inputs.amountLabel')"
-    validation="required|between:1,1000|number"
-    :suffix-icon="!!isRollingShown ? 'tabler:x' : 'tabler:hexagon'"
-    @suffix-icon-click="() => isRollingShown = !isRollingShown"
-  />
-  <AnimationExpand>
-    <DiceRoller
-      v-if="isRollingShown"
-      @rolled="amount = $event, isRollingShown = false"
-    />
-  </AnimationExpand>
+  >
+    <UiFormItem v-auto-animate>
+      <UiFormLabel required>
+        {{ $t('components.inputs.amountLabel') }}
+      </UiFormLabel>
+      <UiFormControl>
+        <UiInputGroup>
+          <UiInputGroupInput
+            type="number"
+            v-bind="componentField"
+          />
+          <UiInputGroupAddon align="inline-end">
+            <UiInputGroupButton
+              :aria-label="isRollingShown ? $t('components.inputs.hideDiceRoller') : $t('components.inputs.showDiceRoller')"
+              @click="isRollingShown = !isRollingShown"
+            >
+              <Icon :name="!!isRollingShown ? 'tabler:x' : 'tabler:hexagon'" />
+            </UiInputGroupButton>
+          </UiInputGroupAddon>
+        </UiInputGroup>
+      </UiFormControl>
+      <DiceRoller
+        v-if="isRollingShown"
+        @rolled="setValue($event), isRollingShown = false"
+      />
+      <UiFormMessage />
+    </UiFormItem>
+  </UiFormField>
 </template>
