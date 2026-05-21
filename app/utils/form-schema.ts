37c8d1bd9bfd10d68e3831567db2f893f0ac46db
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-05-21 15:51:40 +0200

    feat: unit test form schemas

--- app/utils/form-schema.ts
+++ app/utils/form-schema.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
-import { damageTypes, conditionTypes } from '~~/constants/dnd'
+import { damageTypes, conditionTypes, attackTypes, diceTypes, distanceUnits, usageTypes } from '~~/constants/dnd'
+import { abilityType, actionType } from '~~/constants/validation'
 
 export const skillBonusesSchema = z.object({
   acrobatics: z.number().gte(-10).lte(20).optional(),
@@ -65,6 +66,48 @@ export const sightSchema = z.object({
 
 const damageTypeEnum = damageTypes as [DndDamageType, ...DndDamageType[]]
 const conditionTypeEnum = conditionTypes as [DndConditionType, ...DndConditionType[]]
+const attackTypeEnum = attackTypes as [DndAttackType, ...DndAttackType[]]
+const diceTypeEnum = diceTypes as [DndDice, ...DndDice[]]
+const distanceUnitEnum = distanceUnits as [DndDistanceUnit, ...DndDistanceUnit[]]
+const usageTypeEnum = usageTypes as [DndUsageType, ...DndUsageType[]]
+const actionTypeEnum = [...actionType] as [DndActionType, ...DndActionType[]]
+const abilityTypeEnum = [...abilityType] as [DndAbility, ...DndAbility[]]
+
+export const attackSchema = z.object({
+  name: z.string().min(1).max(50),
+  attackType: z.enum(attackTypeEnum),
+  toHitMod: z.number().gte(-20).lte(30).optional(),
+  distanceUnit: z.enum(distanceUnitEnum),
+  reach: z.number().gte(0).lte(1200).optional(),
+  range: z.number().gte(0).lte(1200).optional(),
+  longRange: z.number().gte(0).lte(1200).optional(),
+  targetCreatureOnly: z.boolean().optional(),
+  damageDieCount: z.number().gte(1).lte(100).optional(),
+  damageDieType: z.enum(diceTypeEnum).optional(),
+  damageBonus: z.number().gte(-20).lte(100).optional(),
+  damageType: z.enum(damageTypeEnum).optional(),
+  extraDamageDieCount: z.number().gte(1).lte(100).optional(),
+  extraDamageDieType: z.enum(diceTypeEnum).optional(),
+  extraDamageBonus: z.number().gte(-20).lte(100).optional(),
+  extraDamageType: z.enum(damageTypeEnum).optional(),
+  spellSave: z.number().gte(1).lte(30).optional(),
+  spellSaveType: z.enum(abilityTypeEnum).optional(),
+})
+
+export const usageLimitsSchema = z.object({
+  type: z.enum(usageTypeEnum),
+  param: z.number().gte(1).lte(100),
+})
+
+export const actionSchema = z.object({
+  actionType: z.enum(actionTypeEnum),
+  name: z.string().min(3).max(100),
+  desc: z.string().min(1).max(2000),
+  attacks: z.array(attackSchema).max(10),
+  legendaryActionCost: z.number().gte(1).lte(10).optional(),
+  limitedToForm: z.string().max(50).optional().or(z.literal('')),
+  usageLimits: usageLimitsSchema.optional(),
+})
 
 export const resistancesAndImmunitiesSchema = z.object({
   damageImmunities: z.array(z.enum(damageTypeEnum)).default([]),
